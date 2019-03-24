4acbbdf769f6b42f3de54e517b28b0d7572d45e5
swuerfel <sebawue@gmail.com>
2019-03-24 17:42:00 +0100

    1.7 Added modal with pokemon details

--- JS/oldscriptE7.js
+++ JS/oldscriptE7.js
@@ -26,75 +26,11 @@ var pokemonRepository = (function () {
   function showDetails(item) {
     pokemonRepository.loadDetails(item).then(function () {
       console.log(item);
-      showModal(item.name, item.imageUrl, item.height);
+
+      // Call function showModal
+
     });
   }
-
-
-// modal
-
-var $modalContainer = document.querySelector('#modal-container');
-
-  function showModal(name, img, height) {
-    // Clear all existing modal content
-    $modalContainer.innerHTML = '';
-
-    var modal = document.createElement('div');
-    modal.classList.add('modal');
-
-    // Add the new modal content
-    var closeButtonElement = document.createElement('button');
-    closeButtonElement.classList.add('modal-close');
-    closeButtonElement.innerText = 'Close';
-    closeButtonElement.addEventListener('click', hideModal);
-
-    var titleElement = document.createElement('h1');
-    titleElement.innerText = name;
-
-    var imageElement = document.createElement('img');
-    imageElement.src = img;
-    imageElement.classList.add('pokemon__image');
-
-    var contentElement = document.createElement('p');
-    contentElement.innerText = 'height: ' + height;
-
-    modal.appendChild(closeButtonElement);
-    modal.appendChild(titleElement);
-    modal.appendChild(imageElement);
-    modal.appendChild(contentElement);
-    $modalContainer.appendChild(modal);
-
-    $modalContainer.classList.add('is-visible');
-  }
-
-  function hideModal() {
-    $modalContainer.classList.remove('is-visible');
-  }
-
-  document.querySelector('#show-modal').addEventListener('click', () => {
-    showModal('Modal title', 'This is the modal content!');
-  });
-
-  window.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape' && $modalContainer.classList.contains('is-visible')) {
-      hideModal();
-    }
-  });
-
-  $modalContainer.addEventListener('click', (e) => {
-    // Since this is also triggered when clicking INSIDE the modal container,
-    // We only want to close if the user clicks directly on the overlay
-    var target = e.target;
-    if (target === $modalContainer) {
-      hideModal();
-    }
-  });
-
-// End modal
-
-
-
-
 
 
   //add-Pokemon-Objects function:
@@ -144,9 +80,7 @@ var $modalContainer = document.querySelector('#modal-container');
     addListItem: addListItem,
     // search: search,
     loadList: loadList,
-    loadDetails: loadDetails,
-    hideModal: hideModal,
-
+    loadDetails: loadDetails
   };
 })();
 
